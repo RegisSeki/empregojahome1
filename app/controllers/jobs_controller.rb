808a6f6c5c3e32ce44d5194f146981ce1eedc4a0
RegisSeki <regisseki@gmail.com>
2016-11-16 20:36:32 +0000

    Usuario pode cadastrar uma vaga-feature 4

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -2,4 +2,14 @@ class JobsController < ApplicationController
   def show
     @jobs = Job.find(params[:id])
   end
+  
+  def new
+    @job = Job.new
+  end
+  
+  def create
+    @job = Job.create(params.require(:job)
+              .permit(:title, :location, :category, :company, :description))
+    redirect_to @job 
+  end
 end
